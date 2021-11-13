8e71d724e9d2d5248d21aec7112383b5b895ee86
Ryan Poe <baublet@gmail.com>
2021-11-13 11:41:20 -0600

    Initial commit

---
+++ src/serviceContainer.test.ts
@@ -0,0 +1,89 @@
+import { createServiceContainer, ServiceContainer } from "./serviceContainer";
+
+describe("basic functionality", () => {
+  const testSyncService = () => ({ sync: "service" });
+  const testAsyncService = () =>
+    Promise.resolve({
+      async: "service",
+    });
+  const testLayeredInjection = async (services: ServiceContainer) => {
+    const asyncService = await services.get(testAsyncService);
+    const syncService = services.get(testSyncService);
+    return {
+      asyncService,
+      syncService,
+    };
+  };
+
+  it("creates a basic service container", () => {
+    expect(createServiceContainer).not.toThrow();
+  });
+
+  it("lets us use sync service containers properly", () => {
+    const container = createServiceContainer();
+    const service = container.get(testSyncService);
+    expect(service).toEqual({
+      sync: "service",
+    });
+  });
+
+  it("doesn't re-create containers on subsequent get calls", () => {
+    const container = createServiceContainer();
+    const mockFactory = jest.fn().mockReturnValue("yo");
+    const service = container.get(mockFactory);
+
+    expect(service).toEqual("yo");
+    expect(container.get(mockFactory)).toEqual("yo");
+
+    expect(mockFactory).toHaveBeenCalledTimes(1);
+  });
+
+  it("lets use async service containers properly", () => {
+    const container = createServiceContainer();
+    const service = container.get(testAsyncService);
+    return expect(service).resolves.toEqual({
+      async: "service",
+    });
+  });
+
+  it("passes service factories the container itself, so services can depend on other services", () => {
+    const container = createServiceContainer();
+    return expect(container.get(testLayeredInjection)).resolves.toEqual({
+      syncService: {
+        sync: "service",
+      },
+      asyncService: {
+        async: "service",
+      },
+    });
+  });
+
+  it("allows us to determine which factories have been initialized", () => {
+    const container = createServiceContainer();
+    container.get(testAsyncService);
+
+    expect(container.has(testAsyncService)).toEqual(true);
+    expect(container.has(testSyncService)).toEqual(false);
+  });
+
+  it("allows us to delete specific services from containers", () => {
+    const container = createServiceContainer();
+    container.get(testAsyncService);
+
+    expect(container.has(testAsyncService)).toEqual(true);
+
+    container.delete(testAsyncService);
+
+    expect(container.has(testAsyncService)).toEqual(false);
+  });
+
+  it("lets us prime service containers", () => {
+    const container = createServiceContainer();
+    const primedService = testSyncService();
+    container.set(testSyncService, primedService);
+
+    expect(container.get(testSyncService)).toEqual({
+      sync: "service",
+    });
+  });
+});
---
+++ src/serviceContainer.ts
@@ -0,0 +1,38 @@
+export type ServiceFactory = (serviceContainer: ServiceContainer) => any;
+
+type ServiceFactoryResult<T> = T extends () => infer R ? R : never;
+
+export type ServiceContainer = {
+  get<T extends ServiceFactory>(factory: T): ServiceFactoryResult<T>;
+  set<T extends ServiceFactory>(
+    factory: T,
+    service: ServiceFactoryResult<T>
+  ): ServiceFactoryResult<T>;
+  has(factory: ServiceFactory): boolean;
+  delete(factory: ServiceFactory): void;
+};
+
+export const createServiceContainer: () => ServiceContainer = () => {
+  const serviceMap: Map<ServiceFactory, any> = new Map();
+
+  const serviceContainer: ServiceContainer = {
+    get: (factory) => {
+      if (!serviceMap.has(factory)) {
+        serviceMap.set(factory, factory(serviceContainer));
+      }
+      return serviceMap.get(factory);
+    },
+    set: (factory, service) => {
+      serviceMap.set(factory, service);
+      return service;
+    },
+    has: (factory) => {
+      return serviceMap.has(factory);
+    },
+    delete: (factory) => {
+      serviceMap.delete(factory);
+    },
+  };
+
+  return serviceContainer;
+};
